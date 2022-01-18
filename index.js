4fb7a9f027f05fee1365ae6b4cf0a3cbc07e24e9
Amy Pavel <amypavel@gmail.com>
2022-01-17 22:15:05 -0600

    update for multiple dirs

--- index.js
+++ index.js
@@ -8,24 +8,28 @@ var fs = require('fs');
 */
 
 function plugin(opts){
-  const result = [];
-  const dir = opts.dir;
-  const stats =  fs.lstatSync(dir);
+  const result = {};
+  const dirs = opts.dirs;
 
-  if (stats.isDirectory()) {
-    fs.readdir(dir, function(err, files) {
-      files.sort().forEach(function (file) {
-        if (file.indexOf('.json') !== -1) {
-          const path = dir + file;
-          const basename = file.slice(0, -5);
+  for (const [name, dir] of Object.entries(dirs)) {
+    const resultArr = [];
+    result[name] = resultArr;
+    const stats = fs.lstatSync(dir);
+    if (stats.isDirectory()) {
+      fs.readdir(dir, function(err, files) {
+        files.sort().forEach(function (file) {
+          if (file.indexOf('.json') !== -1) {
+            const path = dir + file;
+            const basename = file.slice(0, -5);
 
-          fs.readFile(path, 'utf8', function (err, data) {
-            if (err) { throw err; }
-            result.push(JSON.parse(data));
-          });
-        }
+            fs.readFile(path, 'utf8', function (err, data) {
+              if (err) { throw err; }
+              resultArr.push(JSON.parse(data));
+            });
+          }
+        });
       });
-    });
+    }
   }
 
   return function(files, metalsmith, done){
